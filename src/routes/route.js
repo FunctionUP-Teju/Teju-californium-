092e766627ae597d5d0cdd8b1a3e5f8cb0805501
FunctionUP-Teju <kapilsharma68@gmail.com>
2022-12-14 01:06:56 +0530

    Day27 Post-Api Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,50 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+////////////////////////////////////////////////////////////////////////////////
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+
+
+router.post("/players" , function (req,res){
+let pack = req.body.name
+let pack2 =req.body
+let bob = players.map(x => x.name)
+if(bob.includes(pack)){
+  return res.send(`Oops...${pack} is already exist`)
+}else{
+  players.push(pack2)
+  return res.send({updatedData : players , status : true})
+}
+})
 module.exports = router;
